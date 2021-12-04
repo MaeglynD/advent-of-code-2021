50f3e3839367d7cca5d7a00934294373e16f9889
MaeglynD <maeglyn.davies@gmail.com>
2021-12-04 12:00:52 +0000

    puzzle 1

---
+++ 1.cpp
@@ -0,0 +1,26 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+	int current, acc = 0, prev = -1;
+	ifstream input;
+	string line;
+	
+	input.open("inputs/1.txt");
+
+	while(getline(input, line)) {
+		current = stoi(line);
+
+		if (prev != -1 && current > prev) {
+			acc++;
+		}
+
+		prev = current;
+	}
+	
+	cout << acc;
+
+	return 0;
+}
