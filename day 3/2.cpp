d8d5c23ab6377840fc94223f05bfe426a9e9211c
MaeglynD <maeglyn.davies@gmail.com>
2021-12-04 18:21:29 +0000

    day 3 pt 2

--- day 3/2.cpp
+++ day 3/2.cpp
@@ -2,29 +2,45 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
+
+string getByCriteria (vector<string> &bytes, string criteria, int pos = 0) {
+	vector<string> a, b;
+
+	for (string &byte: bytes) {
+		(byte[pos] == '1' ? a : b).push_back(byte);
+	}
+
+	size_t as = a.size(), bs = b.size();
+	pos++;
+	
+	vector<string>& chosenVector = criteria == "oxygen" 
+		? (as >= bs ? a : b)
+		: (as >= bs ? b : a);
+
+	if (max(as, bs) == 1) {
+		return chosenVector[0];
+	}
+	
+	return getByCriteria(chosenVector, criteria, pos);
+}
 
 int main () {
 	ifstream input;
-	string line;
-	input.open("../inputs/2.txt");
+	string line, oxygenGeneratorRating, CO2Scrubber;
+	input.open("../inputs/3.txt");
 
-	int aim = 0, horizontalPos = 0, depth = 0;
+	vector<string> report;
 
 	while(getline(input, line)) {
-		size_t pos = line.find(" ");
-		string word = line.substr(0, pos);
-		int amount = stoi(line.substr(pos + 1));
-
-		if (word == "forward") {
-			horizontalPos += amount;
-			depth += (aim * amount);
-		} else {
-			aim += (word == "up" ? -amount : amount);
-		}
+		report.push_back(line);
 	}
-	
-	cout << horizontalPos * depth;
+
+	oxygenGeneratorRating = getByCriteria(report, "oxygen");
+	CO2Scrubber = getByCriteria(report, "co2");
+
+	cout << stoi(oxygenGeneratorRating, nullptr, 2) * stoi(CO2Scrubber, nullptr, 2);
 
 	return 0;
 }
