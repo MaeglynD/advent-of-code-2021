d7216c1e65ed877c024747080f9778bd081c771e
MaeglynD <maeglyn.davies@gmail.com>
2021-12-16 12:59:57 +0000

    day 6 pt 2 dumb recursion way

--- day 6/2.cpp
+++ day 6/2.cpp
@@ -7,16 +7,31 @@
 #include <cstdlib>
 using namespace std;
 
+int getDescendants(int days, int age){
+	int futureDays = days - (age + 1);
+
+	return futureDays >= 0
+		? 1 + getDescendants(futureDays, 6) + getDescendants(futureDays, 8)
+		: 0;
+}
+
 int main () {
-	ifstream input;
+	size_t total = 0;
+	size_t days = 256;
+	vector<int> colony;
+
+	ifstream input("../inputs/6.txt");
 	string line;
 	
-	input.open("../inputs/5.txt");
-
-	while (getline(input, line)) {
-		// 
+	while(getline(input, line, ',')) {
+		colony.push_back(stoi(line));
 	}
 
+	for (int &fish : colony) {
+		total += (1 + getDescendants(days, fish));
+	}
+
+	cout << total;
 
 	return 0;
 }
