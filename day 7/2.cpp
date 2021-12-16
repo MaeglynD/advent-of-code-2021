2be8340e13d0621ab68c75cb8aafeadc8abd1dc1
MaeglynD <maeglyn.davies@gmail.com>
2021-12-16 23:34:34 +0000

    day 7 pt 2

--- day 7/2.cpp
+++ day 7/2.cpp
@@ -4,17 +4,36 @@
 #include <vector>
 #include <sstream>
 #include <algorithm>
-#include <cstdlib>
+#include <cstdlib> 
+#include <numeric>
 using namespace std;
 
 int main () {
+	int minPos, maxPos, optimalFuelUsage = INT_MAX;
+	vector<int> positions;
 
 	ifstream input("../inputs/7.txt");
 	string line;
 	
 	while(getline(input, line, ',')) {
-		// 
+		positions.emplace_back(stoi(line));
 	}
+
+	minPos = *min_element(positions.begin(), positions.end());
+	maxPos = *max_element(positions.begin(), positions.end());
+
+	for (int i = minPos; i <= maxPos; ++i) {
+		int fuelUsage = accumulate(positions.begin(), positions.end(), 0, [&](int& total, int& pos) {
+			int diff = abs(i - pos);
+			return total + (diff * (diff + 1) / 2);
+		});
+
+		if (fuelUsage < optimalFuelUsage) {
+			optimalFuelUsage = fuelUsage;
+		}
+	}
+
+	cout << optimalFuelUsage;
 
 	return 0;
 }
